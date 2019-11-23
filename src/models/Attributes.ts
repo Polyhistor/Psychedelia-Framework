584f848550e6ce71201a0c3215e1361f569c4af5
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-11-23 17:33:33 +1300

    implemented save and fetch and attached the event triggers

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,15 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get<k extends keyof T>(key: k): T[k] {
+  get = <k extends keyof T>(key: k): T[k] => {
     return this.data[key];
-  }
+  };
 
   set(update: T): void {
     Object.assign(this.data, update);
+  }
+
+  getAll(): T {
+    return this.data;
   }
 }
